f239f2e5ff568db8e8f7519d253132712426948b
Carlos Ruiz <carlosruiz44@gmail.com>
2017-11-30 20:27:24 -0500

    made some animations and shit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('#flip-btn').click(function(){
+//toggle the .flipped class on the card// 
+$('.card').toggleClass('flipped');
+});
+
+$('.container').hover(function(){
+//toggle the .flipped class on the card// 
+$('.card').toggleClass('flipped');
+});
